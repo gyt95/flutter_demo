8db0f21b2965c10a845355f1d18e0704bec23e3b
gyt95 <gyt95@163.com>
2023-02-13 23:39:15 +0800

    change layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo', // app title
+      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Todo List'), // page title
+      home: const MyHomePage(title: 'Flutter Todo List'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,64 +30,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<String> list = <String>[];
 
-  void _incrementCounter() {
+  void _submit(value) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      list.add(value);
     });
+    print(list);
   }
+
+  void _update(text, item) {}
+
+  void _remove(item) {}
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title), // appbar title's value comes from MyHomePage
+        title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+          child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'This is Flutter Todo List',
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TextField(onSubmitted: (String value) {
+              _submit(value);
+            }),
+            ListView(
+              children: list.map((item) {
+                return TodoItem(data: item, remove: _remove);
+              }).toList(),
             ),
+            // ListView.builder(
+            //   padding: const EdgeInsets.all(8),
+            //   itemBuilder: (BuildContext context, int index) {
+            //     return Text('1');
+            //     // return Container(
+            //     //   height: 50,
+            //     //   child: Center(child: Text('Entry ${list[index]}')),
+            //     // );
+            //   },
+            //   // separatorBuilder: (BuildContext context, int index) =>
+            //   // const Divider(),
+            // ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )),
     );
+  }
+}
+
+class TodoItem extends StatelessWidget {
+  TodoItem({required this.data, required this.remove})
+      : super(key: ObjectKey(data));
+
+  final String data;
+  final Function(String data) remove;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(title: Text(data));
   }
 }
